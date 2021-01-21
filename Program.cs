58b2707337e7c91486d38552ff7d5730b8935717
ilkay gül <ilkaygl@outlook.com>
2021-01-21 16:48:54 +0300

    Dictionary v1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> listem = new MyDictionary<string>();
+            listem.Add(" - İlkay");
+            listem.Add(" - Salih");
+            listem.Add(" - Ahmet");
+
+            Console.WriteLine(" - Adet : " + listem.Count());
+
+            foreach (var item in listem.Listem)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Liste Oluştu!");
+
+        }
+    }
+}
